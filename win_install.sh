b4692be81dcda5829e28d20257501434511b46cc
Tyler Szabo <tyler.szabo@gmail.com>
2014-05-22 02:48:37 -0700

    Windows Installer - Added Windows installer
      - Support for Vim only

---
+++ win_install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+CYG_USERPROFILE=`cygpath "$USERPROFILE"/`
+
+DATESTAMP=`date +%s`
+
+# Must be Administrator to use MKLINK
+if ! id -G | grep '[^0-9]0[^0-9]' &>/dev/null ; then
+  echo Not Administrator >&2
+  exit 1
+fi
+
+if [ -e "$CYG_USERPROFILE/_vimrc" ] ; then
+  mv "$CYG_USERPROFILE/_vimrc" "$CYG_USERPROFILE/_vimrc.bak-$DATESTAMP"
+fi
+
+if [ -e "$CYG_USERPROFILE/vimfiles" ] ; then
+  mv "$CYG_USERPROFILE/vimfiles" "$CYG_USERPROFILE/vimfiles.bak-$DATESTAMP"
+fi
+
+cmd /c "MKLINK %USERPROFILE%\\_vimrc `cygpath -a -w dotfiles/vimrc`"
+cmd /c "MKLINK /D %USERPROFILE%\\vimfiles `cygpath -a -w dotfiles/vim`"
