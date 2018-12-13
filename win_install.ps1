e72714dbc8de35af8d0050618cc5fded2981d893
Tyler Szabo <tyler.szabo@gmail.com>
2018-12-13 15:05:41 -0800

    Use 2-space indentation in Windows installer

--- win_install.ps1
+++ win_install.ps1
@@ -1,53 +1,53 @@
 [CmdletBinding(SupportsShouldProcess = $True, DefaultParameterSetName="None")]
 Param (
-    [Parameter(Mandatory=$True,Position=1,ParameterSetName="RepoPath")]
-    [ValidateNotNullOrEmpty()]
-    [string]
-    $Repo = (Split-Path -Path $script:MyInvocation.MyCommand.Path -Parent),
+  [Parameter(Mandatory=$True,Position=1,ParameterSetName="RepoPath")]
+  [ValidateNotNullOrEmpty()]
+  [string]
+  $Repo = (Split-Path -Path $script:MyInvocation.MyCommand.Path -Parent),
 
-    [switch]
-    $Overwrite = $False
+  [switch]
+  $Overwrite = $False
 )
 
 $ErrorActionPreference = "Stop";
 
 function LinkDotFile {
-    Param (
-        $ProfileLink,
-        $Link,
-        $DotFile
-    )
+  Param (
+    $ProfileLink,
+    $Link,
+    $DotFile
+  )
 
-    $ArgString = "ProfileLink=$ProfileLink Link=$Link DotFile=$DotFile"
+  $ArgString = "ProfileLink=$ProfileLink Link=$Link DotFile=$DotFile"
 
-    if (-Not $DotFile) { Throw "Target not specified $ArgString" }
+  if (-Not $DotFile) { Throw "Target not specified $ArgString" }
 
-    $Target = (Join-Path -Path $DotFilesRoot -ChildPath $DotFile)
+  $Target = (Join-Path -Path $DotFilesRoot -ChildPath $DotFile)
 
-    if (-Not (Test-Path -Path $Target)) { Throw "Target doesn't exist $ArgString" }
+  if (-Not (Test-Path -Path $Target)) { Throw "Target doesn't exist $ArgString" }
 
-    if ($ProfileLink) {
-        if ($Link) { Throw "Ambiguous link location. $ArgString" }
-        $Link = (Join-Path -Path $env:USERPROFILE -ChildPath $ProfileLink)
-    }
+  if ($ProfileLink) {
+    if ($Link) { Throw "Ambiguous link location. $ArgString" }
+    $Link = (Join-Path -Path $env:USERPROFILE -ChildPath $ProfileLink)
+  }
 
-    if (-Not $Link) { Throw "No link location. $ArgString" }
+  if (-Not $Link) { Throw "No link location. $ArgString" }
 
-    if (Test-Path -Path $Link) {
-        if ($Overwrite) {
-            if (Test-Path $Link -PathType Container) {
-                if ($PSCmdlet.ShouldProcess($Link, "delete")) {
-                    [IO.Directory]::Delete($Link)
-                }
-            } else {
-                Remove-Item -Path $Link -Confirm:$False
-            }
-        } else {
-            Move-Item -Path $Link -Destination "$Link.bak-$TimeStamp"
+  if (Test-Path -Path $Link) {
+    if ($Overwrite) {
+      if (Test-Path $Link -PathType Container) {
+        if ($PSCmdlet.ShouldProcess($Link, "delete")) {
+          [IO.Directory]::Delete($Link)
         }
+      } else {
+        Remove-Item -Path $Link -Confirm:$False
+      }
+    } else {
+      Move-Item -Path $Link -Destination "$Link.bak-$TimeStamp"
     }
+  }
 
-    New-Item -ItemType SymbolicLink -Path $Link -Value $Target
+  New-Item -ItemType SymbolicLink -Path $Link -Value $Target
 }
 
 $DotFilesRoot = Join-Path -Path $Repo -ChildPath "dotfiles"
@@ -57,12 +57,12 @@ $TimeStamp = (Get-Date -UFormat "%s").split(".")[0]
 # Test linking before trying the real thing
 $testfile = (Join-Path -Path $env:TEMP -ChildPath "deleteme-dotfiles-$TimeStamp.tmp")
 try {
-    LinkDotFile -Link $testfile -DotFile "vimrc" | Out-Null
+  LinkDotFile -Link $testfile -DotFile "vimrc" | Out-Null
 } catch { throw } finally {
-    Remove-Item -Path $testfile -Force -ErrorAction SilentlyContinue
+  Remove-Item -Path $testfile -Force -ErrorAction SilentlyContinue
 }
 
-LinkDotFile -ProfileLink "_ackrc"       -DotFile "ackrc"
-LinkDotFile -ProfileLink ".gitconfig"   -DotFile "gitconfig"
-LinkDotFile -ProfileLink "_vimrc"       -DotFile "vimrc"
-LinkDotFile -ProfileLink "vimfiles"     -DotFile "vim"
+LinkDotFile -ProfileLink "_ackrc"     -DotFile "ackrc"
+LinkDotFile -ProfileLink ".gitconfig" -DotFile "gitconfig"
+LinkDotFile -ProfileLink "_vimrc"     -DotFile "vimrc"
+LinkDotFile -ProfileLink "vimfiles"   -DotFile "vim"
