849560a04bf5ef375122af21237e159b13c40266
Tyler Szabo <tyler@szabomail.ca>
2021-09-28 15:41:57 -0700

    Installer - Use backup directory instead of file suffix

--- install.sh
+++ install.sh
@@ -4,11 +4,15 @@ DATESTAMP=`date +%s`
 REPODIR="`dirname \"$0\"`"
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 LOCALE_DIR=$HOME/.locale
+BACKUP_DIR=$HOME/.dotfiles-backups-$DATESTAMP
+BACKUP_DIR_XDG=$BACKUP_DIR/xdg_config_backups
 
 if echo "$REPODIR" | grep '^\.' &>/dev/null ; then
   REPODIR="`echo \"$REPODIR\" | sed -e 's/^\\.//'`"
   REPODIR="`pwd`$REPODIR"
 fi
+
+mkdir -p $BACKUP_DIR $BACKUP_DIR_XDG
 
 for i in "$REPODIR/dotfiles/"* ; do
   FILE=`basename "$i"`
@@ -17,7 +21,7 @@ for i in "$REPODIR/dotfiles/"* ; do
   PATTERN="`sed -e 's:/:\\\\/:g' <<< "$HOME"`"
 
   if [ -e "$DEST" -o -h "$DEST" ] ; then
-    mv "$DEST" "$DEST".bak-$DATESTAMP
+    mv "$DEST" -t "$BACKUP_DIR/"
   fi
 
   ln -s "$i" "$DEST"
@@ -35,14 +39,14 @@ for i in "$REPODIR/xdg_config/"* ; do
   PATTERN="`sed -e 's:/:\\\\/:g' <<< "$HOME"`"
 
   if [ -e "$DEST" -o -h "$DEST" ] ; then
-    mv "$DEST" "$DEST".bak-$DATESTAMP
+    mv "$DEST" -t "$BACKUP_DIR_XDG"
   fi
 
   ln -s "$i" "$DEST"
 done
 
 if [ -e "$LOCALE_DIR" ] ; then
-  mv "$LOCALE_DIR" "$LOCALE_DIR".bak-$DATESTAMP
+  mv "$LOCALE_DIR" -t "$BACKUP_DIR/"
 fi
 mkdir -p "$LOCALE_DIR"
 localedef -f "UTF-8" -i "$REPODIR/locales/en_ZZ" "$LOCALE_DIR/en_ZZ.UTF-8"
